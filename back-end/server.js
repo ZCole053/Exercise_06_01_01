5e316c5ceec1403213e6ca88f7c8cd4f8f6e9d0b
Unknown <zcole053@west-mec.org>
2019-04-22 13:27:20 -0700

    Mongoose
    We started to implement mongoose into our files.

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,17 @@ var express = require('express');
 var app = express();
 var port = 8080;
 var bodyParser = require('body-parser');
+//allows for relationships to be made
+var mongoose = require('mongoose');
+var dbName = 'test';
+//has location of mongo runtime
+var url = 'mongodb://localhost:27017/' + dbName;
+//it is an object
+//param2 = structure of the database
+var Message = mongoose.model('Message', {
+    //putting in datatypes instead
+    msg: String
+});
 
 //midleware
 app.use(bodyParser.json());
@@ -11,6 +22,7 @@ app.use(bodyParser.json());
 
 //next = tells middleware what to do next 
 //used by every route
+//defaulted to route
 app.use((req,res,next) => {
     //setting header to allow cross orign
     //* sets it to be general to make it specific put it in a port
@@ -20,9 +32,34 @@ app.use((req,res,next) => {
 });
 
 
+app.get('/api/message', GetMessages);
+
 app.post('/api/message', (req,res) =>{
     console.log(req.body);
+    //creating new object
+    var message = new Message(req.body);
+    message.save();
     res.status(200);
+});
+
+//crud
+function GetMessages(req, res){
+    //empty gets all records
+    //executes what it finds
+    Message.find({}).exec((err, result) => {
+        res.send(result);
+    });
+}
+
+mongoose.connect(url, (err, db) => {
+    if(err){
+        return console.log('Error: ' + err);
+    }
+    console.log('Connected to database: ' + dbName);
+    // // database.collection('messages').insertOne({
+    // //     'msg': 'test'
+    // // });
+    // GetMessages();//debug
 });
 
 
